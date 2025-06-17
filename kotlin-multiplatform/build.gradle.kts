d5516426391e6bc6fc11c398efec0e23db096bf3
AnthonyPicquet <119321201+AnthonyPicquet@users.noreply.github.com>
2025-06-17 11:20:10 +0200

    feat: [NO-TICKET] update dependencies

--- kotlin-multiplatform/build.gradle.kts
+++ kotlin-multiplatform/build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.Properties
 
 plugins {
     kotlin("multiplatform")
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.20"
     id("com.android.library")
     id("maven-publish")
     kotlin("native.cocoapods")
